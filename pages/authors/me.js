fae631b57add2cdd3ffcb3bd2f31121ec0538a96
victor <v.degermain@gmail.com>
2021-07-21 18:06:49 +0200

    notion de route

---
+++ pages/authors/me.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const me = () => {
+    return (
+        <h1>
+           Nouvelle page 
+        </h1>
+    )
+}
+
+export default me
---
+++ pages/posts/first-post.js
@@ -0,0 +1,14 @@
+import Link from "next/link";
+const firstPost = () => {
+  return (
+    <>
+      <h1>First Post</h1>
+
+      <h2>
+<Link href="/"> Lien fistPost</Link>        
+      </h2>
+    </>
+  );
+};
+
+export default firstPost;
