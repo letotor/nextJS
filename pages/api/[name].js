5eec787c649ebdccc264bb2513e180eb5d0b7e69
victor <v.degermain@gmail.com>
2021-07-27 13:52:04 +0200

    test

--- pages/api/[name].js
+++ pages/api/[name].js
@@ -1,9 +1,11 @@
+const { formatWithValidation } = require("next/dist/next-server/lib/utils");
+
 module.exports = (req, res) => {
   const {
     query: { name },
   } = req;
 
   const val=`Hello ${name}!`+"dfdf"
-  res.send(req);
+  res.send(val);
 
 }
--- pages/api/auth/[...auth0].js
+++ pages/api/auth/[...auth0].js
@@ -1,16 +1,5 @@
 import { handleAuth, handleLogin } from "@auth0/nextjs-auth0";
 
-const getLoginState = (req, loginOptions) => {
-  return { basket_id: "toto" };
-};
 
 export default handleAuth({
-  async login(req, res) {
-    try {
-      await handleLogin(req, res, { getLoginState });
-      console.log("fonction auth");
-    } catch (error) {
-      res.status(error.status || 500).end(error.message);
-    }
-  },
 });
